ebf04655a2d4ac4f3aa5d6b3df5c2ac30400704b
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-06-14 17:48:56 +0800

    addHomeControllerForTesting

---
+++ prod/src/main/java/com/wisata/prod/controller/HomeController.java
@@ -0,0 +1,14 @@
+// filepath: e:\UC_Makassar\Semester 2\ASSESSMENT LEARNING PROJECT\appFiles\Backend-ALP-2025\prod\src\main\java\com\wisata\prod\controller\HomeController.java
+package com.wisata.prod.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the application!";
+    }
+}
