0a2c8a3632761b0212200aebab1a02a2ab755577
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-06-20 22:47:42 +0800

    addingComments

--- prod/src/main/java/com/wisata/prod/controller/FavouriteController.java
+++ prod/src/main/java/com/wisata/prod/controller/FavouriteController.java
@@ -18,12 +18,18 @@ public class FavouriteController {
     private final FavouriteService favouriteService;
 
     @PostMapping
+    // Endpoint to add a favourite destination for a user
+    // Example: http://localhost:8080/api/favourites?userId=1&destinationId=2
+    // Uses User ID and Destination ID as request parameters
     public ResponseEntity<Favourite> addFavourite(@RequestParam Long userId, @RequestParam Long destinationId) {
         Favourite favourite = favouriteService.addFavourite(userId, destinationId);
         return new ResponseEntity<>(favourite, HttpStatus.CREATED);
     }
 
     @GetMapping("{userId}")
+    // TO get all favourites for a specific user
+    // Example: http://localhost:8080/api/favourites/1
+    // Uses User ID as a path variable
     public ResponseEntity<List<Favourite>> getFavouritesByUserId(@PathVariable Long userId) {
         List<Favourite> favourites = favouriteService.getFavouritesByUserId(userId);
         return new ResponseEntity<>(favourites, HttpStatus.OK);
---
+++ prod/src/main/java/com/wisata/prod/entity/Rating.java
@@ -0,0 +1,4 @@
+// package com.wisata.prod.entity;
+
+// public class Rating {    
+// }
