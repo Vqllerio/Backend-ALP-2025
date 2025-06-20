5bcd19445a7217f642859bf157c63b0418e4e0ac
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-06-20 23:34:14 +0800

    loginWorks!!!

--- prod/src/main/java/com/wisata/prod/controller/AuthController.java
+++ prod/src/main/java/com/wisata/prod/controller/AuthController.java
@@ -1,40 +1,40 @@
-package com.wisata.prod.controller;
+// package com.wisata.prod.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.wisata.prod.entity.User;
-import com.wisata.prod.repository.UserRepository;
+// import com.wisata.prod.entity.User;
+// import com.wisata.prod.repository.UserRepository;
 
-@CrossOrigin
-@RestController
-@RequestMapping("/api/auth")
-public class AuthController {
+// @CrossOrigin
+// @RestController
+// @RequestMapping("/api/auth")
+// public class AuthController {
 
-    @Autowired
-    private UserRepository userRepository;
+//     @Autowired
+//     private UserRepository userRepository;
 
-    // REGISTER
-    @PostMapping("/register")
-    public String register(@RequestBody User user) {
-        if (userRepository.findByEmail(user.getEmail()) != null) {
-            return "Email sudah terdaftar!";
-        }
-        userRepository.save(user);
-        return "Akun berhasil dibuat!";
-    }
+//     // REGISTER
+//     @PostMapping("/register")
+//     public String register(@RequestBody User user) {
+//         if (userRepository.findByEmail(user.getEmail()) != null) {
+//             return "Email sudah terdaftar!";
+//         }
+//         userRepository.save(user);
+//         return "Akun berhasil dibuat!";
+//     }
 
-    // LOGIN
-    @PostMapping("/login")
-    public String login(@RequestBody User loginUser) {
-        User user = userRepository.findByEmail(loginUser.getEmail());
-        if (user != null && user.getPassword().equals(loginUser.getPassword())) {
-            return "Login berhasil!";
-        }
-        return "Email atau password salah!";
-    }
-}
+//     // LOGIN
+//     @PostMapping("/login")
+//     public String login(@RequestBody User loginUser) {
+//         User user = userRepository.findByEmail(loginUser.getEmail());
+//         if (user != null && user.getPassword().equals(loginUser.getPassword())) {
+//             return "Login berhasil!";
+//         }
+//         return "Email atau password salah!";
+//     }
+// }
--- prod/src/main/java/com/wisata/prod/repository/UserRepository.java
+++ prod/src/main/java/com/wisata/prod/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.wisata.prod.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wisata.prod.entity.User;
